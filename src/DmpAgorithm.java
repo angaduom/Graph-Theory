eba57a6edadb8abd0d0a59bf91de88b07389ca57
angaduom <angaduom@gmail.com>
2016-03-24 17:50:52 -0400

    adjacency matrix has been made so far next find faces

--- src/DmpAgorithm.java
+++ src/DmpAgorithm.java
@@ -1,11 +1,22 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.StringTokenizer;
 
 /**
  * Created by Angad on 2016-03-24.
  */
 public class DmpAgorithm {
+
+    public void printMatrix(int [][]adj, int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("  "+adj[i][j]+"  ");
+            }
+            System.out.println();
+        }
+    }
 
     public int [][] readFile(String path){
 
@@ -13,10 +24,20 @@ public class DmpAgorithm {
         try{
             BufferedReader br = new BufferedReader(new FileReader(path));
              int adjSize = Integer.parseInt(br.readLine());
-            adj= new int[adjSize][adjSize];
+            adj= new int[adjSize+1][adjSize+1];
             String line;
+            int i=1;
             while ((line = br.readLine()) != null) {
-            System.out.println("line text :"+line);
+                System.out.println("line text :"+line);
+                StringTokenizer st = new StringTokenizer(line," ");
+                while(st.hasMoreTokens()){
+                    int j =Integer.parseInt(st.nextToken());
+                    if(j>0) {
+                        adj[i][j] = 1;
+                        adj[j][i] = 1;
+                    }
+                }
+                i++;
             }
         }
         catch (Exception e){
@@ -29,6 +50,6 @@ public class DmpAgorithm {
         System.out.println("Starting reading the file");
         DmpAgorithm object = new DmpAgorithm();
         int [][] adjacencyMatrix = object.readFile("C:\\Users\\Angad\\software\\Work\\Projects\\Graph-Theory\\graph.txt");
-
+        object.printMatrix(adjacencyMatrix,adjacencyMatrix[0].length);
     }
 }
